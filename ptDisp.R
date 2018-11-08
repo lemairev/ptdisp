259b030b18af0c6fee7164820f8340382c5ecb1b
Vincent Lemaire <vincent.lemaire@polytechnique.edu>
2018-11-07 23:41:30 -0300

    init repo

--- ptDisp.R
+++ ptDisp.R
@@ -5,7 +5,7 @@ library(raster)
 library(RColorBrewer)
 
 # input data
-Q <- 10000  # emissions  (g/s)
+Q <- 10000  # emissions rate (g/s)
 U <- 5      # wind speed (M/s)
 
 # to compute sigma Y,Z ==> cte to fit later (1 val per met class of stability)
@@ -20,28 +20,30 @@ e <- 0.9477
 f <- -0.002
 
 # grid def
-x  <- seq.int(10, 1000, by=10)
-y  <- seq.int(-300, 300, by=10)
-xy <- expand.grid(x=x, y=y, KEEP.OUT.ATTRS=FALSE)
+x  <- seq.int(10, 1000, by = 10)
+y  <- seq.int(-300, 300, by = 10)
+xy <- expand.grid(x = x, y = y, KEEP.OUT.ATTRS=FALSE)
 
 # src pt disp
-calc.sigma <- function(x, c1, c2, c3) { return( exp(c1 + c2*log(x) + c3*(log(x)**2)) ) }
-calc.f     <- function(y, sigmaY) { return( exp(-((y**2) / (2*(sigmaY**2)))) ) }
-calc.conc  <- function(f, Q, U, sigmaY, sigmaZ) { return( Q/U * f/(sigmaY * sigmaZ * pi) ) }
-
-# orrrrrr
-calc.disp <- function(x, y, Q, U, a, b, c, d, e, f) {
-  return( Q/U * exp(-((y**2) / (2*(exp(a + b*log(x) + c*(log(x)**2)))**2))) / (exp(a + b*log(x) + c*(log(x)**2)) * exp(d + e*log(x) + f*(log(x)**2)) * pi) ) 
+calc_sigma <- function(x, c1, c2, c3) { 
+  exp(c1 + c2*log(x) + c3*(log(x)**2)) 
+}
+calc_f     <- function(y, sigmaY) { 
+  exp(-((y**2) / (2*(sigmaY**2)))) 
+}
+calc_conc  <- function(f, Q, U, sigmaY, sigmaZ) { 
+  Q / U * f / (sigmaY * sigmaZ * pi) 
 }
 
 
 # compute conc
-sigY <- calc.sigma(x=xy$x, c1=a, c2=b, c3=c)
-sigZ <- calc.sigma(x=xy$x, c1=d, c2=e, c3=f)
-ffff <- calc.f(y=xy$y, sigmaY=sigY)
-conc <- calc.conc(f=ffff, sigmaY=sigY, sigmaZ=sigZ, Q=Q, U=U)
+sigY  <- calc_sigma(x = xy$x, c1 = a, c2 = b, c3 = c)
+sigZ  <- calc_sigma(x = xy$x, c1 = d, c2 = e, c3 = f)
+ffff  <- calc_f(y = xy$y, sigmaY = sigY)
+conc  <- calc_conc(f = ffff, sigmaY = sigY, sigmaZ = sigZ, Q = Q, U = U)
+
 # agg 2 compare 2 excel file of Fred
-res <- cbind(xy, sigY, ffff, sigZ, conc) 
+res <- cbind(xy, sigY, ffff, sigZ, conc, conca) 
 # plot
 # plot(rasterFromXYZ(xyz=res[,c("x","y","conc")]), breaks=c(1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 5e2), col=rainbow(6) )
 #colr <- brewer.pal(n=9,"BuGn")
